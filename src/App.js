ba83c023219346bbb5cb8528bcfbede6dc2760c5
Kevin Barreto <barretokevin@hotmail.com>
2022-05-30 22:28:16 -0500

    install prettier config in project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 const Pet = (props) => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, props.name),
-        React.createElement('h2', {}, props.animal),
-        React.createElement('h2', {}, props.breed),
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet, { name: 'Figo', animal: 'Dog', breed: 'Shi-Tzu' }),
-        React.createElement(Pet, { name: 'Gigi', animal: 'Dog', breed: 'Chihuahua' }),
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Figo", animal: "Dog", breed: "Shi-Tzu" }),
+    React.createElement(Pet, {
+      name: "Gigi",
+      animal: "Dog",
+      breed: "Chihuahua",
+    }),
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
